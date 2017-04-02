259c422d819d59adcb2880da30f03ca4612d6fdd
Odaine <odaine.forbes@yahoo.com>
2017-04-02 20:42:30 +0000

    ex3

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,1 +1,11 @@
 // Your JavaScript Code here
+
+/* global angular */
+
+var app = angular.module("imageApp", []);
+
+app.controller("imgCtrl", function($scope, $http){
+    $http.get('/api/thumbnails').then(function(response){
+        $scope.images = response.data["thumbnails"];
+    });
+});
