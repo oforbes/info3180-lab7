f0f0c0a3e7ff1c7fd69749a3b89a3930bdcf0875
Odaine <odaine.forbes@yahoo.com>
2017-04-02 10:25:54 +0000

    urls only

--- image-getter.py
+++ image-getter.py
@@ -2,33 +2,36 @@ import requests
 from bs4 import BeautifulSoup
 import urlparse
 
-url = "https://www.walmart.com/ip/54649026"
-result = requests.get(url)
-soup = BeautifulSoup(result.text, "html.parser")
 
-imgs = []
+def getImg():
+    url = "https://www.walmart.com/ip/54649026"
+    result = requests.get(url)
+    soup = BeautifulSoup(result.text, "html.parser")
 
-# This will look for a meta tag with the og:image property
-og_image = (soup.find('meta', property='og:image') or
+    imgs = []
+
+    # This will look for a meta tag with the og:image property
+    og_image = (soup.find('meta', property='og:image') or
                     soup.find('meta', attrs={'name': 'og:image'}))
-if og_image and og_image['content']:
+    if og_image and og_image['content']:
     #print og_image['content']
     #print ''
-    pass
+        pass
 
 # This will look for a link tag with a rel attribute set to 'image_src'
 
-thumbnail_spec = soup.find('link', rel='image_src')
-if thumbnail_spec and thumbnail_spec['href']:
-    pass
+    thumbnail_spec = soup.find('link', rel='image_src')
+    if thumbnail_spec and thumbnail_spec['href']:
+        pass
     #print thumbnail_spec['href']
     #print ''
 
 
-#image = """<img src="%s"><br />"""
-for img in soup.findAll("img", src=True):
-   #print image % urlparse.urljoin(url, img["src"])
-   #print ''
-   if img["src"] not in imgs:
-       imgs.append(img["src"])
-return imgs
+    #image = """<img src="%s"><br />"""
+    for img in soup.findAll("img", src=True):
+        #print image % urlparse.urljoin(url, img["src"])
+        #print ''
+        if img["src"] not in imgs:
+            imgs.append(img["src"])
+       
+    return imgs
